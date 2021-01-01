e1d84db6a9908c29d3bb3f051c18cdf0fc5bec73
”honda_yurika” <rain.is.capriccio@gmail.com>
2021-01-01 08:50:45 +0000

    childモデルのテスト記述変更
    わかりやすいようにグループ分けした

--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -9,25 +9,30 @@ RSpec.describe Child, type: :model do
    expect(FactoryBot.build(:child, name: "")).to_not be_valid
  end
  
- it "is invalid without a login_id" do
-   expect(FactoryBot.build(:child, login_id: "")).to_not be_valid
+ describe "login_idについてのバリデーションチェック" do
+   it "is invalid without a login_id" do
+     expect(FactoryBot.build(:child, login_id: "")).to_not be_valid
+   end
+   
+   
+   it "login_idが重複していたら登録できない" do
+     child1 = FactoryBot.create(:child, name: "taro", login_id: "aaa")
+     expect(FactoryBot.build(:child, name: "ziro", login_id: child1.login_id)).to_not be_valid
+   end
  end
  
- it "login_idが重複していたら登録できない" do
-   child1 = FactoryBot.create(:child, name: "taro", login_id: "aaa")
-   expect(FactoryBot.build(:child, name: "ziro", login_id: child1.login_id)).to_not be_valid
- end
- 
- it "is invalid without a password" do
-   expect(FactoryBot.build(:child, password: "")).to_not be_valid
- end
- 
- it "パスワードが暗号化されているか" do
-   child = FactoryBot.create(:child)
-   expect(child.password_digest).to_not eq "password1"
- end
- 
- it "password_confirmationとpasswordが異なる場合保存できない" do
-   expect(FactoryBot.build(:child, password:"password1",password_confirmation: "passward1")).to_not be_valid
+ describe "passwordについてのバリデーション チェック" do
+   it "is invalid without a password" do
+     expect(FactoryBot.build(:child, password: "")).to_not be_valid
+   end
+   
+   it "パスワードが暗号化されているか" do
+     child = FactoryBot.create(:child)
+     expect(child.password_digest).to_not eq "password1"
+   end
+   
+   it "password_confirmationとpasswordが異なる場合保存できない" do
+     expect(FactoryBot.build(:child, password:"password1",password_confirmation: "passward1")).to_not be_valid
+   end
  end
 end
